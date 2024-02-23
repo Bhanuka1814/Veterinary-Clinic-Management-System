c4a0c3ebcd178b308e90a9bf12139be3a8a5aa74
Bhanuka1814 <161048316+Bhanuka1814@users.noreply.github.com>
2024-02-23 20:29:51 +0530

    Add files via upload

---
+++ ViewPetList.java
@@ -0,0 +1,18 @@
+
+package veterinary_clinic1;
+
+
+
+public class ViewPetList {
+
+    
+   public void  viewPet(){
+       PetRegister register =new PetRegister();
+    System.out.println("Pet name :"+register.pet_name);
+    System.out.println("Owwner name :"+register.owner_name);
+    System.out.println("Owner Address :"+register.owner_address);
+    System.out.println("PetAge :"+register. age);
+    System.out.println("Owner Telephone Number :"+register.owner_tp);
+   
+   } 
+}
